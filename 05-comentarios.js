20ac232c0e93a1621079e48e069272621284689d
hugoVasquez345 <166969741+hugoVasquez345@users.noreply.github.com>
2024-05-10 23:19:41 -0600

    Add files via upload

---
+++ 05-comentarios.js
@@ -0,0 +1,14 @@
+// notehu nthoeunhtoeu
+/*
+Todo lo que escribamos entermedio
+de los slash y los asteriscos
+va a ser ignorado
+*/
+/**
+ * Este es un comentario 
+ * de muchas 
+ * lineas
+ */
+let numero =  42; // Este es otro comentario 
+
+console.log(numero);
---
+++ 07-for-of.js
@@ -0,0 +1,9 @@
+// while 
+// do while 
+// for
+
+let animales = ['Chachito feliz','Dragon', 'Perrito'];
+
+for (let animal of animales) {
+    console.log(animal);
+}
